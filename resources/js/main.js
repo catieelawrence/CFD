846403976f3750b04298cc996def647ed2e987b6
Lawrence Rodriguez <CLAWRENCERODRIGUEZ@mgh.harvard.edu>
2018-01-11 13:22:39 -0500

    adding initial content

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(document).ready(function () {
+  $('.carousel').slick({
+   arrows: false,
+   dots: true,
+   autoplay: true,
+   autoplaySpeed: 3000,
+   pauseOnHover: true,
+   fade: true
+ });
+});
