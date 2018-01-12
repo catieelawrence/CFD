a8cdb1b3d31df343ccff122addda30fd133be432
Lawrence Rodriguez <CLAWRENCERODRIGUEZ@mgh.harvard.edu>
2018-01-12 12:27:46 -0500

    added carousel

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,10 @@
 // JavaScript Document
 
 $(document).ready(function () {
-  $('.carousel').slick({
-   arrows: false,
-   dots: true,
-   autoplay: true,
-   autoplaySpeed: 3000,
-   pauseOnHover: true,
-   fade: true
+	$('.frame').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		fade: true
  });
 });
